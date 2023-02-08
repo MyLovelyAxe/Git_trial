53fdbc8fa4c86aeafc4bb0a645a05f26b8f29863
hardli <1060111768@qq.com>
2023-02-08 15:55:02 +0100

    first commit

---
+++ import_numpy_as_np.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def swap(a, b):
+    a, b = b, a
+    return a, b
+
+a = 23
+b = 43
+
+print(a)
+print(b)
---
+++ numpy001.py
@@ -0,0 +1,7 @@
+import numpy as np
+a = np.arange(12).reshape(3,4)
+b = np.ones_like(a)
+c = np.vstack((a,b))
+d = np.hstack((a,b))
+print(c)
+print(d)
